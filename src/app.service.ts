a16cc6e1519d2e1d7779763cb0fc62832ae12cf8
David Hype <iamsocialhype@gmail.com>
2026-08-01 16:42:54 +0100

    chore: configured paystack on server

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome Page</title>
+  <title>Welcome To This Page</title>
   <style>
     body {
       margin: 0;
